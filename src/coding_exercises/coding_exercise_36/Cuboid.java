1ff7fe80bcc11ea71abdcf0022e56d3258aa4dce
Ivan Petkov <ipetkow90@gmail.com>
2021-02-19 23:24:55 +0200

    Solving following coding exercises: - Coding Exercise 35: Cylinder - Coding Exercise 36: Pool Area

---
+++ src/coding_exercises/coding_exercise_36/Cuboid.java
@@ -0,0 +1,19 @@
+package coding_exercises.coding_exercise_36;
+
+public class Cuboid extends Rectangle {
+
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
+}
