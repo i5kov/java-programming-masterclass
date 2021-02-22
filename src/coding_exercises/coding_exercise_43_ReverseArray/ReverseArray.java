4589330140ad6e5f85922e635397dee7a97b1f67
Ivan Petkov <ipetkow90@gmail.com>
2021-02-22 23:31:41 +0200

    Solving following coding exercises: - Coding Exercise 42: Minimum Element - Coding Exercise 43: Reverse Array

---
+++ src/coding_exercises/coding_exercise_43_ReverseArray/ReverseArray.java
@@ -0,0 +1,21 @@
+package coding_exercises.coding_exercise_43_ReverseArray;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        int[] array = {5, 4, 3, 2, 1};
+        reverse(array);
+    }
+
+    private static void reverse(int[] array) {
+        System.out.println("Array = " + Arrays.toString(array));
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        System.out.println("Reversed array = " + Arrays.toString(array));
+    }
+}
