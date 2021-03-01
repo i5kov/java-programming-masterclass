f17044563fbe1f89ce32bcaaeda52ae030e3241a
Ivan Petkov <ipetkow90@gmail.com>
2021-03-01 22:04:11 +0200

    Section 10: Generics - Solving generic challenges

---
+++ src/s10_generics/generics_challenge/BaseballPlayer.java
@@ -0,0 +1,9 @@
+package s10_generics.generics_challenge;
+
+
+public class BaseballPlayer extends Player {
+
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ src/s10_generics/generics_challenge/SoccerPlayer.java
@@ -0,0 +1,9 @@
+package s10_generics.generics_challenge;
+
+
+public class SoccerPlayer extends Player {
+
+    public SoccerPlayer(String name) {
+        super(name);
+    }
+}
